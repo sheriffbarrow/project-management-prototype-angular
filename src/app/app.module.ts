b6adde75aef53dca186fd141384c186d767dc37c
¨sheriffbarrow¨ <smsbarrowjnr@gmail.com>
2023-07-19 21:28:42 +0000

    initial  commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { SettingsComponent } from './settings/settings.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    DashboardComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    SettingsComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
